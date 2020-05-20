8d3cf8350cf01eddd649a351459bde85eeafaafb
HencoSmith <b0sbeertjie007@gmail.com>
2020-05-20 17:50:41 +0200

    Add: initial project files still a lot to be done

---
+++ src/index.js
@@ -0,0 +1,39 @@
+/**
+ * @description App for playing around with Jexia, Main Entry point
+ * @file /src/index.js
+ * @author Henco Smith 2020
+ */
+
+/**
+ * Packages
+ */
+// Time helpers
+const moment = require('moment');
+// Command line formatting
+const chalk = require('chalk');
+// Jexia
+const { jexiaClient, dataOperations, UMSModule } = require('jexia-sdk-js/node');
+
+
+/**
+ * Program entry
+ */
+(() => {
+    console.info(chalk.yellow(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Program Start`));
+
+    /**
+     * Inits
+     */
+    const dataOps = dataOperations();
+    const ums = new UMSModule();
+
+    const projectID = process.env.PROJECT_ID;
+
+    console.info(chalk.yellow(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Init ID '${projectID}'`));
+    jexiaClient().init({
+        projectID,
+    }, dataOps, ums);
+
+    // TODO: jexia integration
+    // TODO: aribnb linter setup
+})();
