13edad24e4665ba10d60df1a8510096652fb1e5d
HencoSmith <b0sbeertjie007@gmail.com>
2020-05-22 15:03:33 +0200

    Fix: eslint ecma version Add: functionality to pull chuck norris joke from api and push to jexia dataset

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # jexia-playground
-Testing and playing with Jexia https://www.jexia.com/en/
+Pushes Chuck norris joke to Jexia dataset every time app runs.
+[Jexia](https://www.jexia.com/en/)
 
 # Environment Variables
 * JEXIA_PROJECT_ID - Found on Jexia Settings used in DB connection
+* JEXIA_API_KEY - API key used to access datasets
+* JEXIA_API_SECRET - Secret associated with API key
+
+# startup
+```bash
+npm i
+npm start
+```
--- src/index.js
+++ src/index.js
@@ -11,28 +11,76 @@
 const moment = require('moment');
 // Command line formatting
 const chalk = require('chalk');
+// General helper library
+const _ = require('lodash');
 // Jexia
-const { jexiaClient, dataOperations, UMSModule } = require('jexia-sdk-js/node');
+const { jexiaClient, dataOperations } = require('jexia-sdk-js/node');
+// REST helper
+const axios = require('axios');
+// Config file loader
+const config = require('config');
+// Promise helper
+const Promise = require('bluebird');
 
 
 /**
  * Program entry
  */
-(() => {
-    console.info(chalk.yellow(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Program Start`));
+(async () => {
+    try {
+        console.info(chalk.yellow(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Program Start`));
 
-    /**
-     * Inits
-     */
-    const dataOps = dataOperations();
-    const ums = new UMSModule();
+        // Test Env definitions
+        const projectID = process.env.JEXIA_PROJECT_ID;
+        if (_.isUndefined(projectID) || _.isNull(projectID)) {
+            throw new Error('JEXIA_PROJECT_ID environment variable must be defined');
+        }
+        const key = process.env.JEXIA_API_KEY;
+        if (_.isUndefined(key) || _.isNull(key)) {
+            throw new Error('JEXIA_API_KEY environment variable must be defined');
+        }
+        const secret = process.env.JEXIA_API_SECRET;
+        if (_.isUndefined(secret) || _.isNull(secret)) {
+            throw new Error('JEXIA_API_SECRET environment variable must be defined');
+        }
 
-    const projectID = process.env.JEXIA_PROJECT_ID;
 
-    console.info(chalk.yellow(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Init ID '${projectID}'`));
-    jexiaClient().init({
-        projectID,
-    }, dataOps, ums);
+        // Pull a Chuck Norris joke
+        const { data } = await axios.get(config.get('request.chuckNorrisGet'));
+        const joke = data.value;
+        console.info(chalk.green(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] '${joke}'`));
 
-    // TODO: jexia
+
+        // Jexia Setup
+        const dataOps = dataOperations();
+
+        console.info(chalk.yellow(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Init Project ID '${projectID}'`));
+
+        jexiaClient().init({
+            projectID,
+            key,
+            secret,
+        }, dataOps);
+
+        // Push Joke to dataset
+        const jokes = dataOps.dataset(config.get('jexia.jokeDataset'));
+
+        const insertQuery = jokes.insert([{
+            description: joke,
+        }]);
+
+        const records = await new Promise((resolve, reject) => {
+            insertQuery.subscribe((res) => {
+                resolve(res);
+            }, (err) => {
+                reject(err);
+            });
+        }).catch((err) => { throw err; });
+
+        console.info(chalk.green(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${JSON.stringify(records)}`));
+    } catch (err) {
+        console.error(chalk.red(err.message));
+        console.info(chalk.yellow(err.stack));
+        process.exit(1);
+    }
 })();
